1519e244ddf3f8a57fb7ab588f4f4f88625fa83f
Lilly Walther <lilly@rehtlaw.me>
2024-09-20 20:08:20 +0200

    some reflex changes

--- BGAnimations/ScreenTestReflex underlay/default.lua
+++ BGAnimations/ScreenTestReflex underlay/default.lua
@@ -4,13 +4,13 @@ local InputHandler = function( event )
 	if not event then return end
 
 	if event.type == "InputEventType_FirstPress" then
-		
+
 		Trace(event.DeviceInput.button)
-		
+
 		if event.DeviceInput.button == "DeviceButton_3" then
 			if not reflexhelp_hidden then
 				REFLEX:Connect()
-				REFLEX:SetPanelThresholds(300,300,300,300)
+				REFLEX:SetPanelThresholds(270,200,165,270)
 				REFLEX:SetPanelCooldowns(60,60,60,60)
 			end
 		elseif event.DeviceInput.button == "DeviceButton_4" then
@@ -31,7 +31,7 @@ local InputHandler = function( event )
 		elseif event.DeviceInput.button == "DeviceButton_enter" then
 			SCREENMAN:SetNewScreen('ScreenSelectProfile') 
 		end
-		
+
 	end
 
 end
@@ -49,7 +49,7 @@ local function addbar(obj)
 	local xv = table.getn(reflex_bars)
 
 	table.insert(reflex_bars,obj)
-	
+
 	local bg = obj:GetChild('bg')
 	local level1 = obj:GetChild('level1')
 	local level2 = obj:GetChild('level2')
@@ -60,17 +60,17 @@ local function addbar(obj)
 	local v2 = obj:GetChild('value2')
 	local v3 = obj:GetChild('value3')
 	local v4 = obj:GetChild('value4')
-	
+
 	x = (xv + 1) * sw/5
-	
+
 	obj:x(x)
 	obj:y(sh - 100)
-	
+
 	bg:valign(1)
 	bg:zoomto(16,280)
 	bg:diffuse(0,.5,1,.3)
 	bg:diffusetopedge(1,.5,0,.3)
-	
+
 	level1:blend('add')
 	level1:diffuse(1,0,0,.7)
 	level1:zoomto(32,4)
@@ -86,7 +86,7 @@ local function addbar(obj)
 	level5:blend('add')
 	level5:diffuse(1,1,1,0)
 	level5:zoomto(24,4)
-	
+
 	v1:y(20)
 	v1:zoom(.4)
 	v2:y(30)
@@ -95,7 +95,7 @@ local function addbar(obj)
 	v3:zoom(.4)
 	v4:y(50)
 	v4:zoom(.4)
-	
+
 end
 
 function math.clamp(val,min,max)
@@ -139,9 +139,9 @@ local t = Def.ActorFrame {
 		screen:AddInputCallback( InputHandler )
 	end,
 	InitCommand=function(self)
-	
+
 		GAMESTATE:JoinPlayer(0)
-		
+
 	end
 }
 
@@ -157,15 +157,15 @@ local t = Def.ActorFrame {
 		StepP1RightLiftMessageCommand="%function(self) diffusebarbg(3,.3) end"
 	/>
 	]]
-	
+
 t[#t+1] = Def.BitmapText {
 	Font="_eurostile outline",
 	Text="Test Reflex :D (1.1)",
 	OnCommand=function(self) self:zoom(1.5):xy(scx,84) end
 }
 
-for i=0,3 do
-	
+for _=0,3 do
+
 	t[#t+1] = Def.ActorFrame {
 		OnCommand=function(self) addbar(self) end,
 		Def.Quad { Name="bg" },
@@ -179,39 +179,44 @@ for i=0,3 do
 		Def.BitmapText { Font="_eurostile outline", Text="0", Name="value3" },
 		Def.BitmapText { Font="_eurostile outline", Text="0", Name="value4" }
 	}
-	
+
 end
-	
+
 t[#t+1] = Def.Actor {
 	OnCommand=function(self) self:queuecommand("Update") end,
 	UpdateCommand=function(self)
-		
+
 		if not reflexhelp_hidden then
-		
-			local sens = {4200/5700,5000/5700,5000/5700,5300/5700}
-			
+
+			local sens = {
+				(REFLEX:GetPanelBaseline(0)+REFLEX:GetPanelThreshold(0))/5700,
+				(REFLEX:GetPanelBaseline(1)+REFLEX:GetPanelThreshold(1))/5700,
+				(REFLEX:GetPanelBaseline(2)+REFLEX:GetPanelThreshold(2))/5700,
+				(REFLEX:GetPanelBaseline(3)+REFLEX:GetPanelThreshold(3))/5700
+			}
+
 			local pressure = hardware_readPressureNormalizedPersonal()
-			
+
 			for i=0,3 do
-			
+
 				positionbar(i,1,REFLEX:GetPanelValue(i),2500,8000)
 				positionbar(i,2,REFLEX:GetPanelBaseline(i),2500,8000)
 				positionbar(i,3,REFLEX:GetPanelBaseline(i)+REFLEX:GetPanelThreshold(i),2500,8000)
 				positionbar(i,4,REFLEX:GetPanelBaseline(i)+REFLEX:GetPanelThreshold(i)-REFLEX:GetPanelCooldown(i),2500,8000)
-				
+
 				bar_max[i+1] = math.max(REFLEX:GetPanelValue(i),bar_max[i+1])
 				positionbar(i,5,bar_max[i+1],2500,8000)
-				
+
 				settexti(i,1,REFLEX:GetPanelValue(i))
 				settexti(i,2,REFLEX:GetPanelValueAboveBaseline(i))
 				settexti(i,3,REFLEX:GetPanelBaseline(i))
 				settexti(i,4,bar_max[i+1]-REFLEX:GetPanelBaseline(i))
-				
+
 				local val = math.floor(4000*pressure[i+1]/sens[i+1])
 				REFLEX:SetLightArrow(0,i,math.clamp(255*val/5700,0,255)*0.1,0,math.clamp(255*val/5700,0,255)*0.2)
-				
+
 			end
-			
+
 		end
 
 		self:sleep(0.02)
