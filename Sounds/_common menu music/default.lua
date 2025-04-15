ac93491204d93418dcba16c46b59cf5f8584c0b6
Lilly Walther <lilly@rehtlaw.me>
2025-04-16 01:47:30 +0200

    make entry screen music random, add more songs

--- Sounds/_common menu music/default.lua
+++ Sounds/_common menu music/default.lua
@@ -1,10 +1,9 @@
 -- plan is to have one set menu song but i'll keep this logic because it could allow some neat things anyway
-local songs = {
-	Waterfall = "Aquatic"
-}
+local songs = {"Aquatic", "Beanmania IIDX", "feel", "halcyon farms", "in_two", "Whispers Under Ground"}
 
 -- use the style to index the songs table (above)
-local file = songs.Waterfall
+local randNum = math.random(#songs)
+local file = songs[randNum]
 
 -- if a song file wasn't defined in the songs table above
 -- fall back on the song for Hearts as default music
